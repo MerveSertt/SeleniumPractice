daece0899914586fa21b786998c2a135593f0ee7
merve <mervebalkan34@gmail.com>
2023-05-29 20:29:11 +0300

    Initial commit

---
+++ src/test/java/day02_practice.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day02_practice {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+
+    /*
+
+    2. Youtube ana sayfasina gidelim . https://
+    www.youtube.com/
+    3. Amazon soyfasina gidelim. https://www.amazon.com/
+    4. Tekrar YouTube’sayfasina donelim
+    5. Yeniden Amazon sayfasina gidelim
+    6. Sayfayi Refresh(yenile) yapalim
+    7. Sayfayi kapatalim / Tum sayfalari kapatalim
+
+     */
+
+    driver.get("https://www.youtube.com");
+    driver.get("https://www.amazon.com");
+    driver.navigate().back();
+    driver.navigate().forward();
+    driver.navigate().refresh();
+    driver.close();
+    driver.quit();
+
+
+    }
+}
---
+++ src/test/java/day02_practice_2.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day02_practice_2 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        /*
+        2. Amazon soyfasina gidelim. https://www.amazon.com/
+        3. Sayfanin konumunu ve boyutlarini yazdirin
+        4. Sayfayi simge durumuna getirin
+        5. simge durumunda 3 saniye bekleyip sayfayi maximize yapin
+        6. Sayfanin konumunu ve boyutlarini maximize durumunda
+        yazdirin
+        7. Sayfayi fullscreen yapin
+        8. Sayfanin konumunu ve boyutlarini fullscreen durumunda
+        yazdirin
+        9. Sayfayi kapatin
+         */
+
+        driver.get("https://www.amazon.com");
+        System.out.println("Sayfanın Konumu: " + driver.manage().window().getPosition());
+        System.out.println("Sayfanın Boyutları: " + driver.manage().window().getSize());
+        driver.manage().window().minimize();
+        Thread.sleep(3000);
+        driver.manage().window().maximize();
+        System.out.println("Sayfanın Maximize Konumu: " +driver.manage().window().getPosition());
+        System.out.println("Sayfanın Maximize Boyutları: " +driver.manage().window().getSize());
+        driver.manage().window().fullscreen();
+        System.out.println("Sayfanın Full Screen Konumu: " +driver.manage().window().getPosition());
+        System.out.println("Sayfanın Full Screen Boyutları: " +driver.manage().window().getSize());
+        driver.close();
+
+
+    }
+}
