25c35dc23c2016aaa9c73b1de5b89623f6bbf335
merve <mervebalkan34@gmail.com>
2023-05-31 23:36:28 +0300

    31.05.2023

---
+++ src/test/java/day04_practice_1.java
@@ -0,0 +1,12 @@
+public class day04_practice_1 {
+    public static void main(String[] args) {
+        //Techproeducation sayfasına gidelim
+        //Çıkan reklamı locate edip kapatalım
+        //Arama bölümünde qa aratalım
+        //Sayfa başlığının qa içerdiğini doğrulayalım
+        //Carrer Opportunities In QA linkinin görünür ve erişilebilir olduğunu doğrulayalım
+        //Carrer Opportunities In QA linkine tıklayalım
+        //Başlığın Opportunities içerdiğini test edelim
+        //Tekrar anasayfaya dönelim ve url'in https://techproeducation.com/ olduğunu doğrulayalım
+    }
+}
