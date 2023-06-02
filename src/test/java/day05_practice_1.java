dac6f58a43edc57fd835c501bccfe4ffdbe1a6b7
merve <mervebalkan34@gmail.com>
2023-06-02 20:29:32 +0300

    01.06.2023 Odev

---
+++ src/test/java/day05_practice_1.java
@@ -0,0 +1,78 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class day05_practice_1 {
+      /*
+                ODEV
+
+            // http://the-internet.herokuapp.com/add_remove_elements/ adresine gidiniz
+            // Add Element butonuna 100 defa basınız
+            // 100 defa basıldığını test ediniz
+            // 90 defa delete butonuna basınız
+            // 90 defa basıldığını doğrulayınız
+            // Sayfayı kapatınız
+         */
+      public static void main(String[] args) {
+
+          System.setProperty("WebDriver.Chrome.Driver","src/resources/drivers/chromedriver.exe");
+
+          WebDriver driver = new ChromeDriver();
+
+          driver.manage().window().maximize();
+          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+          // http://the-internet.herokuapp.com/add_remove_elements/ adresine gidiniz
+          driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+
+          // Add Element butonuna 100 defa basınız
+          WebElement addButton = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
+
+          int counter = 0;
+
+          for (int i = 0; i < 100; i++) {
+              addButton.click();
+              counter++;
+          }
+
+          // 100 defa basıldığını test ediniz
+          if (counter==100){
+              System.out.println("Add Button Test Passed");
+          }else {
+              System.out.println("Add Button Test Failed");
+          }
+
+
+          // 90 defa delete butonuna basınız
+          List<WebElement> deleteButton = driver.findElements(By.xpath("//button[@class='added-manually']"));
+
+            int counterDelete = 0;
+          for (int i = 0; i < 90; i++) {
+              deleteButton.get(i).click();
+              counterDelete++;
+          }
+          // 90 defa basıldığını doğrulayınız
+          if (counterDelete==90){
+              System.out.println("Delete Button Test Passed");
+          }else {
+              System.out.println("Delete Button Test Failed");
+          }
+
+          // Sayfayı kapatınız
+          driver.close();
+
+
+
+
+
+
+
+
+
+
+      }
+}
---
+++ src/test/java/day05_practice_2.java
@@ -0,0 +1,90 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class day05_practice_2 {
+    /*
+    1-C01_TekrarTesti isimli bir class olusturun
+    2- https://www.amazon.com/ adresine gidin
+    3- Browseri tam sayfa yapin
+    4.- Sayfayi “refresh” yapin
+    5. Sayfa basliginin “Spend less” ifadesi icerdigini test edin
+    6. Gift Cards sekmesine basin
+    7. Birthday butonuna basin
+    8. Best Seller bolumunden ilk urunu tiklayin
+    9. Gift card details’den 25 $’i secin
+    10-Urun ucretinin 25$ oldugunu test edin
+    10-Sayfayi kapatin
+     */
+    public static void main(String[] args) {
+
+
+        System.setProperty("WebDriver.Chrome.Driver","src/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+        //2- https://www.amazon.com/ adresine gidin
+        driver.get("https://www.amazon.com");
+
+        //3- Browseri tam sayfa yapin
+        driver.manage().window().fullscreen();
+
+        //4- Sayfayi “refresh” yapin
+        driver.navigate().refresh();
+
+        //5. Sayfa basliginin “Spend less” ifadesi icerdigini test edin
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Spend less";
+
+        if (actualTitle.contains(expectedTitle)){
+            System.out.println("Title Test");
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+        //6. Gift Cards sekmesine basin
+        WebElement giftCards = driver.findElement(By.xpath("(//a[@class='nav-a  ']) [4]"));
+        giftCards.click();
+
+        //7. Birthday butonuna basin
+        WebElement birthdayButton = driver.findElement(By.xpath("//*[text()='Birthday']"));
+        birthdayButton.click();
+
+        //Best Seller bolumunden ilk urunu tiklayin
+        WebElement bestSeller = driver.findElement(By.xpath("//*[text()='Best Sellers']"));
+        bestSeller.click();
+        WebElement ilkUrun = driver.findElement(By.xpath("//*[@alt='Amazon Reload']"));
+        ilkUrun.click();
+
+
+        //9. Gift card details’den 25 $’i secin
+        WebElement ucret = driver.findElement(By.xpath("(//button[@name='selectAmount']) [1]"));
+        ucret.click();
+
+
+        //10-Urun ucretinin 25$ oldugunu test edin
+        String actual = ucret.getText();
+        String expected = "25 $";
+
+        if (actual.contains(expected)){
+            System.out.println("Ücret Testi");
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Test Failed");
+        }
+
+        //10-Sayfayi kapatin
+        driver.close();
+
+
+
+    }
+}
